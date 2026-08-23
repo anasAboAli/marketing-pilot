4e2303b6fad797666a695cf762187d4f53b7c3ad
Anas <anas.akram8143863@gmail.com>
2026-08-23 15:19:15 +0300

    Prepare fullstack project for deployment

--- server/server.js
+++ server/server.js
@@ -13,10 +13,8 @@ const PORT = process.env.PORT || 3001;
 // Middleware
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://market-pilot-fullstack.netlify.app",
-    ],
+    origin: true,
+    credentials: true,
   })
 );
 
