4967261b73cbaf5a4dbdac3b566776be53877d93
Anas <anas.akram8143863@gmail.com>
2026-08-27 14:30:20 +0300

    Update login page

--- src/pages/auth/LoginPage.vue
+++ src/pages/auth/LoginPage.vue
@@ -4,85 +4,66 @@ import { useRouter } from "vue-router";
 import authService from "@/services/auth.service";
 import { useAuthStore } from "@/stores/auth";
 
-const router=useRouter();
+const router = useRouter();
 
-const auth=useAuthStore();
+const auth = useAuthStore();
 
-const email=ref("");
+const email = ref("");
 
-const password=ref("");
+const password = ref("");
 
-async function login(){
+async function login() {
+  const response = await authService.login({
+    email: email.value,
+    password: password.value,
+  });
 
-    const response=await authService.login({ email: email.value, password: password.value });
+  auth.login(response.user, response.token);
 
-    auth.login(response.user,response.token);
-
-    router.push("/");
-
+  router.push("/");
 }
 </script>
 
 <template>
+  <div class="flex min-h-screen items-center justify-center bg-slate-100">
+    <div class="w-full max-w-md rounded-2xl bg-white p-8 shadow">
+      <h1 class="mb-8 text-center text-3xl font-bold">تسجيل الدخول</h1>
 
-<div class="flex min-h-screen items-center justify-center bg-slate-100">
+      <div class="mb-5">
+        <label class="mb-2 block"> البريد الإلكتروني </label>
 
-<div class="w-full max-w-md rounded-2xl bg-white p-8 shadow">
+        <input
+          value="userAdmin"
+          v-model="email"
+          type="email"
+          class="w-full rounded-xl border px-4 py-3"
+        />
+      </div>
 
-<h1 class="mb-8 text-center text-3xl font-bold">
+      <div class="mb-8">
+        <label class="mb-2 block"> كلمة المرور </label>
 
-تسجيل الدخول
+        <input
+          value="123456789"
+          v-model="password"
+          type="password"
+          class="w-full rounded-xl border px-4 py-3"
+        />
+      </div>
 
-</h1>
+      <button
+        @click="login"
+        class="w-full rounded-xl bg-blue-600 py-3 text-white"
+      >
+        دخول
+      </button>
 
-<div class="mb-5">
-
-<label class="mb-2 block">
-
-البريد الإلكتروني
-
-</label>
-
-<input
-v-model="email"
-type="email"
-class="w-full rounded-xl border px-4 py-3">
-
-</div>
-
-<div class="mb-8">
-
-<label class="mb-2 block">
-
-كلمة المرور
-
-</label>
-
-<input
-v-model="password"
-type="password"
-class="w-full rounded-xl border px-4 py-3">
-
-</div>
-
-<button
-@click="login"
-class="w-full rounded-xl bg-blue-600 py-3 text-white">
-
-دخول
-
-</button>
-
-<RouterLink
-to="/forgot-password"
-class="mt-5 block text-center text-blue-600">
-
-نسيت كلمة المرور؟
-
-</RouterLink>
-
-</div>
-
-</div>
-
+      <RouterLink
+        to="/forgot-password"
+        class="mt-5 block text-center text-blue-600"
+      >
+        نسيت كلمة المرور؟
+      </RouterLink>
+    </div>
+  </div>
 </template>
